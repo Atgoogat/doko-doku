b988a5de5eec21ebd6ea5bdaa6c81a3a2a0c746e
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2022-07-23 09:39:45 +0200

    fix: expression changed error

--- src/app/doko/new-game/new-game.component.ts
+++ src/app/doko/new-game/new-game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core'
 import {
   FormArray,
   FormBuilder,
@@ -23,20 +23,24 @@ export class NewGameComponent implements OnInit {
     ]),
   })
 
-  constructor() {}
+  constructor(private readonly changeRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {}
 
   addPlayer() {
     this.formGroup.controls['playerNames'].push(this.getPlayerNameFormControl())
+    this.changeRef.detectChanges()
   }
 
   removePlayerAt(index: number) {
     this.formGroup.controls['playerNames'].removeAt(index)
+    this.changeRef.detectChanges()
   }
 
   createGame() {
-    console.log(this.formGroup.getRawValue())
+    if (this.formGroup.valid) {
+      console.log(this.formGroup.getRawValue())
+    }
   }
 
   private getPlayerNameFormControl() {
